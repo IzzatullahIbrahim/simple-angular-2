79ab85035028fcaf7be8bf18618c94ab14db7936
Zack <izzatullahibrahim@yahoo.com>
2016-12-20 11:37:25 -0700

    Added navigation, controller, and scope

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var app = angular.module ('MyApp',['ui.router']);
+
+app.config([
+    '$stateProvider',
+    function ($stateProvider){
+        $stateProvider
+            .state('home',{
+            url:'/',
+            templateUrl:'/templates/home.html',
+            controller:'MyApp.HomeController'
+            })
+            .state('about',{
+            url:'/about',
+            templateUrl:'/templates/about.html'
+            })
+            .state('contact',{
+            url:'/contact',
+            templateUrl:'/templates/contact.html'
+            })
+    }
+])
+
+//Create a home page controller.
+app.controller ('MyApp.HomeController',[
+    //list the dependecies for this controller.
+    '$scope',
+
+    //Provide a callback to run once all the dependecies have been created
+    function($scope){
+        console.log('The home controller has loaded')
+        // The scope object acts as a public area that is avaiable to the markup
+        // page and other objects or services.
+        $scope.name = 'bob';
+        $scope.age= 32;
+
+        $scope.increaseAge = function () {
+            console.log('increasing the Age');
+            $scope.age = $scope.age+1;
+        }
+    }
+]);
